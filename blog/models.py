1f90043c49534bafb0adc2941dc90e98f97cd9b9
UncleAyo <ayosamobitade@gmail.com>
2022-11-26 21:30:58 +0100

    some Updates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChoosePanel
+from wagtail.admin.edit_handlers import FieldPanel
+
+class BlogPage(Page):
+    description = models.Charfield(max_length = 255, blank = True,)
+    content_panels = Page.content_panels + [FieldPanel("description", classname = 'full')]
+
+
+    class PostPage(Page):
+        header_mage = models.Foreignkey('wagtailimages.Image', null = True, on_delete = models.SET_NULL, related_name = '+', )
+        conntent_panels = Pages.content_panels + [ImagesChooserPanel('header_image'),]
